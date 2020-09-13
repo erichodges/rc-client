17d31483c8d4515e2f51ec44aa87cae2c22be534
Eric Hodges <ehodges2004@gmail.com>
2020-09-13 15:36:08 -0700

    Setup: Added urql and graphql and setup in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { createClient, Provider } from 'urql';
 import theme from '../theme';
 
+const client = createClient({ url: 'http://localhost:4444/graphql' });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
-  )
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
 export default MyApp;
