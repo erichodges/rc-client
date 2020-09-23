1f2b983b0520739907eabade2ee4a0f48bcfbbe1
Eric Hodges <ehodges2004@gmail.com>
2020-09-23 13:42:58 -0700

    Chore: Cleanded up imports on betterUpdateQuery

--- src/utills/betterUpdateQuery.tsx
+++ src/utills/betterUpdateQuery.tsx
@@ -1,4 +1,4 @@
-import { QueryInput } from '@urql/exchange-graphcache';
+import { Cache, QueryInput } from '@urql/exchange-graphcache';
 
 export function betterUpdateQuery<Result, Query>(
   cache: Cache,
