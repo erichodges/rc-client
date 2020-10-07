dc7e925bd69760ea238f76ba1e8fb850edc96113
Eric Hodges <ehodges2004@gmail.com>
2020-10-07 15:42:55 -0700

    Setup: Added has more button and related code in createUrqlClient,pages/index, and post.graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import { withUrqlClient } from 'next-urql';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout } from '../components/Layout';
 import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
+  const [variables, setVariables] = useState({
+    limit: 4,
+    cursor: null as null | string
+  });
+
+  // console.log(variables);
+
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+    variables
   });
 
   if (!fetching && !data) {
@@ -28,7 +33,7 @@ const Index = () => {
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data?.posts.map((p) => (
+          {data?.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -36,9 +41,20 @@ const Index = () => {
           ))}
         </Stack>
       )}
-      {data ? (
+      {data && data.posts.hasMore ? (
         <Flex>
-          <Button isLoading={fetching} m="auto" my={8}>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt
+              });
+            }}
+            variantColor="teal"
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
             More...
           </Button>
         </Flex>
