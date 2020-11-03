31688fe1bd18e4e371dca8f384874a027471b62a
Eric Hodges <ehodges2004@gmail.com>
2020-11-02 16:13:26 -0800

    Setup: Added voteStatus to PostSnippet fragment

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -43,6 +43,7 @@ export type Post = {
   title: Scalars['String'];
   text: Scalars['String'];
   points: Scalars['Float'];
+  voteStatus?: Maybe<Scalars['Int']>;
   creatorId: Scalars['String'];
   creator: User;
   createdAt: Scalars['String'];
@@ -141,7 +142,7 @@ export type UsernamePasswordInput = {
 
 export type PostSnippetFragment = (
   { __typename?: 'Post' }
-  & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'points' | 'textSnippet'>
+  & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'points' | 'voteStatus' | 'textSnippet'>
   & { creator: (
     { __typename?: 'User' }
     & Pick<User, 'id' | 'username'>
@@ -288,6 +289,7 @@ export const PostSnippetFragmentDoc = gql`
   updatedAt
   title
   points
+  voteStatus
   textSnippet
   creator {
     id
