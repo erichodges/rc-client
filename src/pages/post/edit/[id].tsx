57eac29ca3d76d4a47a690332ed515dd93cbef8a
Eric Hodges <ehodges2004@gmail.com>
2020-11-09 14:39:58 -0800

    Chore: updated formatting on [id].tsx

--- src/pages/post/edit/[id].tsx
+++ src/pages/post/edit/[id].tsx
@@ -1,16 +1,16 @@
-import { Box, Button } from "@chakra-ui/core";
-import { Form, Formik } from "formik";
-import { withUrqlClient } from "next-urql";
-import { useRouter } from "next/router";
-import React from "react";
-import { InputField } from "../../../components/InputField";
-import { Layout } from "../../../components/Layout";
+import { Box, Button } from '@chakra-ui/core';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { InputField } from '../../../components/InputField';
+import { Layout } from '../../../components/Layout';
 import {
   usePostQuery,
   useUpdatePostMutation
-} from "../../../generated/graphql";
-import { createUrqlClient } from "../../../utils/createUrqlClient";
-import { useGetIntId } from "../../../utils/useGetIntId";
+} from '../../../generated/graphql';
+import { createUrqlClient } from '../../../utils/createUrqlClient';
+import { useGetIntId } from '../../../utils/useGetIntId';
 
 const EditPost = ({}) => {
   const router = useRouter();
@@ -39,7 +39,7 @@ const EditPost = ({}) => {
   }
 
   return (
-    <Layout variant="small">
+    <Layout variant='small'>
       <Formik
         initialValues={{ title: data.post.title, text: data.post.text }}
         onSubmit={async (values) => {
@@ -49,20 +49,20 @@ const EditPost = ({}) => {
       >
         {({ isSubmitting }) => (
           <Form>
-            <InputField name="title" placeholder="title" label="Title" />
+            <InputField name='title' placeholder='title' label='Title' />
             <Box mt={4}>
               <InputField
                 textarea
-                name="text"
-                placeholder="text..."
-                label="Body"
+                name='text'
+                placeholder='text...'
+                label='Body'
               />
             </Box>
             <Button
               mt={4}
-              type="submit"
+              type='submit'
               isLoading={isSubmitting}
-              variantColor="teal"
+              variantColor='teal'
             >
               update post
             </Button>
