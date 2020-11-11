13850c99ae3f0905a2dc75e8f33fde4f1ab74f47
Eric Hodges <ehodges2004@gmail.com>
2020-11-11 14:21:07 -0800

    Setup: Made index page reload on logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Flex, Heading, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useRouter } from "next/router";
 import React from 'react';
 import { useLogoutMutation, useUserQuery } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
@@ -7,6 +8,7 @@ import { isServer } from '../utils/isServer';
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useUserQuery({
     pause: isServer()
@@ -39,8 +41,9 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         </NextLink>
         <Box mr={2}>{data.user.username}</Box>
         <Button
-          onClick={() => {
-            logout();
+          onClick={async () => {
+            await logout();
+            router.reload();
           }}
           isLoading={logoutFetching}
           variant="link"
